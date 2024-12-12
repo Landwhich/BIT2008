e6078289409a7a212e28e4a64b7517ae779e1d19
landwhich <will.f.richards@gmail.com>
2024-12-11 20:19:17 -0500

    init

--- messager/models.py
+++ messager/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# class AltUser(AbstractUser):
-#     friends = models.ManyToManyField("self", blank=True)
-#
-#     def add_friend(self, friend_user):
-#         self.friends.add(friend_user)
-#         friend_user.friends.add(self)
-#
-#     def remove_friend(self, friend_user):
-#         self.friends.remove(friend_user)
-#         friend_user.friends.remove(self)
+class AltUser(AbstractUser):
+    friends = models.ManyToManyField("self", blank=True)
+
+    def add_friend(self, friend_user):
+        self.friends.add(friend_user)
+        friend_user.friends.add(self)
+
+    def remove_friend(self, friend_user):
+        self.friends.remove(friend_user)
+        friend_user.friends.remove(self)
 
 # class Room(models.Model):
 #     name = models.CharField(max_length=50, unique=True)
@@ -23,8 +23,8 @@ class Text(models.Model):
     # room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='texts', default=None)
     date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    # sender = models.ForeignKey(AltUser, related_name="sender" ,on_delete=models.CASCADE, default=None)
-    # receiver = models.ForeignKey(AltUser, related_name="receiver", on_delete=models.CASCADE, default=None)
+    sender = models.ForeignKey(AltUser, related_name="sender" ,on_delete=models.CASCADE, default=None)
+    receiver = models.ForeignKey(AltUser, related_name="receiver", on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return "f{self.content}, {self.date}"
